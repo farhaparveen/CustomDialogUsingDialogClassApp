eeb5f37da1bb6346578933970e3ee0fdd82fb1c1
Farha Parveen <FarhaP@hcl.com>
2019-05-24 11:11:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/customdialogusingdialogclassapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.customdialogusingdialogclassapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
